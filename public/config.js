5f1cfa127d82aaf1501865a7bc20de1da66d37d0
bohesocool <1352811656@qq.com>
2025-12-31 18:50:17 +0800

    修复Docker配置和config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,8 @@
 // 运行时配置注入
 // Docker 启动时会替换这个文件中的占位符
-window.__APP_CONFIG__ = {
-  AUTH_PASSWORD: '__AUTH_PASSWORD__'
-};
-
-// 如果是占位符，则设为 undefined
-if (window.__APP_CONFIG__.AUTH_PASSWORD === '__AUTH_PASSWORD__') {
-  window.__APP_CONFIG__.AUTH_PASSWORD = undefined;
-}
+(function() {
+  var password = '__AUTH_PASSWORD__';
+  window.__APP_CONFIG__ = {
+    AUTH_PASSWORD: password === '__AUTH_PASSWORD__' ? undefined : password
+  };
+})();
