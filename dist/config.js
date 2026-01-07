836566ed0cec8b59cf40e90d4b68f8e34edf3a1e
bohesocool <1352811656@qq.com>
2026-01-08 01:19:34 +0800

    feat: 优化 Docker 环境密码配置及 .gitignore 规则

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-// 运行时配置注入
-// Docker 启动时会替换这个文件中的占位符
-(function() {
-  var password = '__AUTH_PASSWORD__';
-  window.__APP_CONFIG__ = {
-    AUTH_PASSWORD: password === '__AUTH_PASSWORD__' ? undefined : password
-  };
-})();
